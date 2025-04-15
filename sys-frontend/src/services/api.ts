be064586617983632a8caf8dbdc2331dfc4d8c86
KumaloWilson <kumalowilson900@gmail.com>
2025-04-15 13:00:34 +0200

    deploying the frontend

--- sys-frontend/src/services/api.ts
+++ sys-frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 import toast from "react-hot-toast"
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://requisition-system-web.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
